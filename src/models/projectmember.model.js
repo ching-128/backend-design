e8423dead3c44cc61488c5e2f3d6a935642f7bee
ching <prasadgowda128@gmail.com>
2025-08-15 17:28:52 +0530

    feat: add user and project models with validation and relationships
    - Added bcryptjs and jsonwebtoken dependencies for authentication.
    - Implemented Project model with fields: name, description, and createdBy.
    - Created ProjectMember model with user, project, and role fields.
    - Developed SubTask model with title, task, isCompleted, and createdBy fields.
    - Enhanced Task model with title, description, project, assignedTo, status, and attachments.
    - Established User model with fields for fullname, email, username, password, and various tokens.
    - Added password hashing and token generation methods to User model.
    - Created empty controller files for auth, healthcheck, note, project, and task.

--- src/models/projectmember.model.js
+++ src/models/projectmember.model.js
@@ -1,5 +1,30 @@
 import mongoose, { Schema } from "mongoose";
+import {AvailableUserRoles, UserRolesEnum} from "../utils/constants.js"
 
-const projectMemberSchema = new Schema({});
+const projectMemberSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        project: {
+            type: Schema.Types.ObjectId,
+            ref: "Porject",
+            required: true,
+        },
+        role: {
+            type: String,
+            enum: UserRolesEnum,
+            default: UserRolesEnum.MEMBER,
+        }
+    },
+    {
+        timestamps: true,
+    },
+);
 
-export const ProjectMember = mongoose.model("ProjectMember", projectMemberSchema);
+export const ProjectMember = mongoose.model(
+    "ProjectMember",
+    projectMemberSchema,
+);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,115 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+    {
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        username: {
+            type: String,
+            required: true,
+            lowercase: true,
+            unique: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false,
+        },
+        forgotPasswordToken: {
+            type: String,
+        },
+        forgotPasswordExpiry: {
+            type: Date,
+        },
+        refreshtoken: {
+            type: String,
+        },
+        emailVarificationToken: {
+            type: String,
+        },
+        emailVarificationExpiry: {
+            type: Date,
+        },
+        avatar: {
+            type: {
+                url: String,
+                localpath: String,
+            },
+            default: {
+                url: "https://placehold.co/600x400",
+                localpath: "",
+            },
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+    jwt.sign(
+        {
+            _id: this.id,
+            email: this.email,
+            username: this.username,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        },
+    );
+};
+userSchema.methods.generateRefreshToken = async function () {
+    jwt.sign(
+        {
+            _id: this.id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        },
+    );
+};
+
+userSchema.methods.generateTemporaryToken = function () {
+    const unHashedToken = crypto.randomBytes(20).toString("hex");
+
+    const hashedToken = crypto
+        .createHash("256")
+        .update(unHashedToken)
+        .digest("hex");
+    const tokenExpiry = Date.now() + 20 * 60 * 1000; //20min
+
+    return { unHashedToken, hashedToken, tokenExpiry };
+};
 
 export const User = mongoose.model("User", userSchema);
