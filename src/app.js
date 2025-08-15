2884d3c6ba2fd5140dce4c62df732f3c89b95f11
ching <prasadgowda128@gmail.com>
2025-08-15 17:44:26 +0530

    feat: add health check route and initialize route files for notes, projects, and tasks

--- src/app.js
+++ src/app.js
@@ -2,4 +2,11 @@ import express from "express";
 
 const app = express();
 
+// middlewares
+
+// routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 export default app;
